943a9e245bbf5108e0d7d47adb5bbef7708d7454
satwikkamath <107809929+satwikkamath@users.noreply.github.com>
2023-08-07 21:12:03 +0530

    added mongoose models

--- index.js
+++ index.js
@@ -15,6 +15,67 @@ app.get("/",function(req,res){
     res.render("chat");
 })
 
+
+//Connecting with MongoDB Database
+
+mongoose.connect("mongodb+srv://satwikroopa:Roopa70263@fruitdb.8sxipgz.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true });
+
+// Schedule template creation
+
+const scheduleSchema = new mongoose.Schema({
+    doctorName: String,
+    date: String,
+    slot1: String,
+    slot2: String,
+    slot3: String,
+})
+
+// Doctor data template creation
+
+const docSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    phoneNumber: Number,
+    password: String,
+    address: String,
+    age: Number,
+    gender: String,
+    experience: Number,
+    specialization: String,
+    area: String,
+    schedule: [scheduleSchema],
+});
+
+//User Data Template Creation
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+    phoneNumber: Number,
+    age: Number,
+    gender: String,
+})
+
+//Appointment Template Creation
+
+const appointmentSchema = new mongoose.Schema({
+    doctorName: String,
+    userName: String,
+    status: String,
+    date: String,
+    slot: String,
+    type: String,
+    address: String
+})
+
+const Doctor = mongoose.model("doctor", docSchema);
+const User = mongoose.model("user", userSchema);
+const Appointment = mongoose.model("appointment", appointmentSchema);
+const Schedule = mongoose.model("schedule", scheduleSchema);
+
+
+
 app.get("/createSession", function (req, res) {
     
     const url = "http://api.endlessmedical.com/v1/dx/InitSession";
@@ -27,6 +88,11 @@ app.get("/createSession", function (req, res) {
         })
     })
 })
+
+
+
+
+
 
 app.listen(3000,function(){
     console.log("Server running")
